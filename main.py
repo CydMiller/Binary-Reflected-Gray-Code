2febbb7168137b214cbdc8e9858bab3c6c77a570
CYDNEY MILLER <cgmiller42@tntech.edu>
2025-11-22 00:15:58 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+    Filename: main.py
+    Author: Cydney Miller
+    Date Created: 11/22/2025
+    Date Updated: 11/22/2025
+    Purpose: Driver for Binary Reflected Gray Code educational tool. 
+'''
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
